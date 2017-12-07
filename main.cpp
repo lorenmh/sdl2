b50d3b8ee51bb49560501d464fd937895f0dbbc3
lorenmh <neutronstar@gmail.com>
2017-12-07 10:55:57 -0800

    getting triangle to render

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <GL/glew.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/glu.h>
 #include <SDL2/SDL.h>
 
 int main() {
@@ -7,6 +8,12 @@ int main() {
         std::cout << "Error encountered initializing" << std::endl
                   << SDL_GetError();
     };
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
     SDL_Window *window = SDL_CreateWindow(
             "Name",
@@ -17,21 +24,49 @@ int main() {
             SDL_WINDOW_OPENGL
     );
 
-    if (window == NULL) {
+    if (window == nullptr) {
         std::cout << "Error creating window: " << std::endl << SDL_GetError();
     }
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    std::cout << "OpenGL version: " <<  glGetString(GL_VERSION);
 
     SDL_GL_SetSwapInterval(1);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
 
     glClearColor(1.0, 0.0, 0.0, 1.0);
+
+    GLfloat vertices[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(vertexArray);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //SDL_GL_SwapWindow(window);
+
+
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    //SDL_GL_SwapWindow(window);
+    //glDisableVertexAttribArray(0);
+
     SDL_GL_SwapWindow(window);
 
     SDL_Delay(5000);
