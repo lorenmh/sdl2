4117c3500171948d5442c1a316170d64bbfe9e6d
lorenmh <neutronstar@gmail.com>
2017-12-07 01:24:16 -0800

    OpenGL works!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
 
 int main() {
@@ -13,18 +14,29 @@ int main() {
             SDL_WINDOWPOS_UNDEFINED,
             640,
             480,
-            SDL_WINDOW_SHOWN
+            SDL_WINDOW_OPENGL
     );
 
-    SDL_Surface *windowSurface = SDL_GetWindowSurface(window);
-    SDL_Surface *imageSurface = SDL_LoadBMP("assets/test.bmp");
-
-    if (imageSurface == NULL) {
-        std::cout << "Error loading surface: " << std::endl << SDL_GetError();
+    if (window == NULL) {
+        std::cout << "Error creating window: " << std::endl << SDL_GetError();
     }
 
-    SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
-    SDL_UpdateWindowSurface(window);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_GL_SetSwapInterval(1);
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(window);
 
     SDL_Delay(5000);
+
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
