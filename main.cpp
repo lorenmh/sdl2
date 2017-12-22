7b06ef944361d67e65196581d05823afd1458c5f
lorenmh <neutronstar@gmail.com>
2017-12-22 14:42:40 -0800

    change fragment shader from tut

--- main.cpp
+++ main.cpp
@@ -41,9 +41,9 @@ bool init_resources() {
     const GLchar* fs_sources[] = {
             version,
             "void main(void) {        "
-                    "  gl_FragColor[0] = 0.0; "
-                    "  gl_FragColor[1] = 0.0; "
-                    "  gl_FragColor[2] = 1.0; "
+                    "  gl_FragColor[0] = gl_FragCoord.x/640.0;"
+                    "  gl_FragColor[1] = gl_FragCoord.y/480.0;"
+                    "  gl_FragColor[2] = 0.5; "
                     "}"
     };
     glShaderSource(fs, 2, fs_sources, NULL);
