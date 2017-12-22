ba3bbce17a27be0d9dfaaa1fb819bf212ba1bf5e
lorenmh <neutronstar@gmail.com>
2017-12-22 14:39:17 -0800

    Triangle rendered from https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Introduction , need to use OpenGL 2.1 ?

--- main.cpp
+++ main.cpp
@@ -1,77 +1,147 @@
 #include <iostream>
 #include <OpenGL/gl3.h>
-#include <OpenGL/glu.h>
+#include <OpenGL/gl3ext.h>
 #include <SDL2/SDL.h>
 
-int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "Error encountered initializing" << std::endl
-                  << SDL_GetError();
+using namespace std;
+
+GLuint program;
+GLint attribute_coord2d;
+
+bool init_resources() {
+    GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+
+    // GLSL version
+    const char* version;
+    int profile;
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, &profile);
+    if (profile == SDL_GL_CONTEXT_PROFILE_ES)
+        version = "#version 100\n";  // OpenGL ES 2.0
+    else
+        version = "#version 120\n";  // OpenGL 2.1
+
+    const GLchar* vs_sources[] = {
+            version,
+            "attribute vec2 coord2d;                  "
+                    "void main(void) {                        "
+                    "  gl_Position = vec4(coord2d, 0.0, 1.0); "
+                    "}"
     };
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-    SDL_Window *window = SDL_CreateWindow(
-            "Name",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            640,
-            480,
-            SDL_WINDOW_OPENGL
-    );
-
-    if (window == nullptr) {
-        std::cout << "Error creating window: " << std::endl << SDL_GetError();
+    glShaderSource(vs, 2, vs_sources, NULL);
+    glCompileShader(vs);
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &compile_ok);
+    if (!compile_ok) {
+        cerr << "Error in vertex shader" << endl;
+        return false;
     }
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
-
-    std::cout << "OpenGL version: " <<  glGetString(GL_VERSION);
-
-    SDL_GL_SetSwapInterval(1);
-    glDisable(GL_DEPTH_TEST);
-    glDisable(GL_CULL_FACE);
-
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-
-    GLfloat vertices[] = {
-            -1.0f, -1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            0.0f,  1.0f, 0.0f,
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar* fs_sources[] = {
+            version,
+            "void main(void) {        "
+                    "  gl_FragColor[0] = 0.0; "
+                    "  gl_FragColor[1] = 0.0; "
+                    "  gl_FragColor[2] = 1.0; "
+                    "}"
     };
+    glShaderSource(fs, 2, fs_sources, NULL);
+    glCompileShader(fs);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &compile_ok);
+    if (!compile_ok) {
+        cerr << "Error in fragment shader" << endl;
+        return false;
+    }
 
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
+    if (!link_ok) {
+        cerr << "Error in glLinkProgram" << endl;
+        return false;
+    }
 
-    GLuint vertexArray;
-    glGenVertexArrays(1, &vertexArray);
-    glBindVertexArray(vertexArray);
+    const char* attribute_name = "coord2d";
+    attribute_coord2d = glGetAttribLocation(program, attribute_name);
+    if (attribute_coord2d == -1) {
+        cerr << "Could not bind attribute " << attribute_name << endl;
+        return false;
+    }
 
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBindVertexArray(vertexArray);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    //SDL_GL_SwapWindow(window);
-
-
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
-    //SDL_GL_SwapWindow(window);
-    //glDisableVertexAttribArray(0);
-
-    SDL_GL_SwapWindow(window);
-
-    SDL_Delay(5000);
-
-    SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    return true;
 }
+
+void render(SDL_Window* window) {
+    /* Clear the background as white */
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(program);
+    glEnableVertexAttribArray(attribute_coord2d);
+    GLfloat triangle_vertices[] = {
+            0.0,  0.8,
+            -0.8, -0.8,
+            0.8, -0.8,
+    };
+    /* Describe our vertices array to OpenGL (it can't guess its format automatically) */
+    glVertexAttribPointer(
+            attribute_coord2d, // attribute
+            2,                 // number of elements per vertex, here (x,y)
+            GL_FLOAT,          // the type of each element
+            GL_FALSE,          // take our values as-is
+            0,                 // no extra data between each position
+            triangle_vertices  // pointer to the C array
+    );
+
+    /* Push each element in buffer_vertices to the vertex shader */
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(attribute_coord2d);
+
+    /* Display the result */
+    SDL_GL_SwapWindow(window);
+}
+
+void free_resources() {
+    glDeleteProgram(program);
+}
+
+void mainLoop(SDL_Window* window) {
+    while (true) {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev)) {
+            if (ev.type == SDL_QUIT)
+                return;
+        }
+        render(window);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    /* SDL-related initialising functions */
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("My First Triangle",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    SDL_GL_CreateContext(window);
+
+
+    /* When all init functions run without errors,
+       the program can initialise the resources */
+    if (!init_resources())
+        return EXIT_FAILURE;
+
+    /* We can display something if everything goes OK */
+    mainLoop(window);
+
+    /* If the program exits in the usual way,
+       free resources and exit with a success */
+    free_resources();
+    return EXIT_SUCCESS;
+}
+
+
